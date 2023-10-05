42d90300097a2c3e58f30909a6c2fd72146b8f62
achmadshafry <achmadshafry@gmail.com>
2023-10-05 16:56:40 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,3 @@
-// import 'dart:html';
-
-// import 'dart:html';
-
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
